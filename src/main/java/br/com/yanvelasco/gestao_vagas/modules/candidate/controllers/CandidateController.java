84b9236c782c67ff55d8fa0c8a0d7d2c33dc400b
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-09 16:53:03 -0300

    Projeto inicializado

---
+++ src/main/java/br/com/yanvelasco/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,14 @@
+package br.com.yanvelasco.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    public void create(){
+
+    }
+
+}
