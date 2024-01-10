620aa8427e7716948ada67fad35fe8a97863c6f0
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-10 13:25:24 -0300

    Creiando a entidade Candidates nno banco de dados

--- src/main/java/br/com/yanvelasco/gestao_vagas/modules/candidate/entity/CandidateEntity.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/modules/candidate/entity/CandidateEntity.java
@@ -1,29 +1,49 @@
 package br.com.yanvelasco.gestao_vagas.modules.candidate.entity;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
+@Table(name = "candidates")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     @NotBlank(message = "O campo [nome] não deve estar em branco")
     private String name;
+
     @NotBlank(message = "O campo [username] não deve estar em branco")
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços em branco")
     private String username;
+
     @Email(message = "O campo [email] deve conter um email válido")
     private String email;
+
     @Length(min = 8, message = "O campo [password] deve ter no minímo 8 caracteres")
     private String password;
+
     @NotBlank(message = "O campo [username] não deve estar em branco")
     private String description;
+
     @NotBlank(message = "O campo [username] não deve estar em branco")
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }
