d26524ea2bc61a52c5dd7e2e7a251c9cf669e5ef
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-11 14:11:53 -0300

    Sobrescrevendo as configurações do SecurityFilterChain

---
+++ src/main/java/br/com/yanvelasco/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package br.com.yanvelasco.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+
+}
