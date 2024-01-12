e562bb5eec7acb503a3edb11af5ecedcd9792def
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-12 13:08:40 -0300

    Criptrografando a senha do CandidateEntity

--- src/main/java/br/com/yanvelasco/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.yanvelasco.gestao_vagas.exceptions.UserAlreadyExists;
 import br.com.yanvelasco.gestao_vagas.modules.candidate.entity.CandidateEntity;
 import br.com.yanvelasco.gestao_vagas.modules.candidate.repository.CandidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidadeRepository candidadeRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity){
         candidadeRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent(user ->{
                     throw new UserAlreadyExists("O usuário ja está cadastrado");
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return candidadeRepository.save(candidateEntity);
     }
 
