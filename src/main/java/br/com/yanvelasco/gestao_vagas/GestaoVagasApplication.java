d92bd0cf7679df7e5b9ecd185d7a361ae0ecddbb
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-15 14:19:07 -0300

    Configurando o Swagger

--- src/main/java/br/com/yanvelasco/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/GestaoVagasApplication.java
@@ -1,9 +1,18 @@
 package br.com.yanvelasco.gestao_vagas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Gestão de Vagas",
+				description = "API responsável pela gestão de vagas",
+				version = "1"
+		)
+)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/yanvelasco/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/security/SecurityConfig.java
@@ -20,6 +20,12 @@ public class SecurityConfig {
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
 
+    private static final String[] SWAGGER_LIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**"
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable())
@@ -28,7 +34,8 @@ public class SecurityConfig {
                     auth.requestMatchers("/candidate").permitAll()
                             .requestMatchers("/company").permitAll()
                             .requestMatchers("/company/auth").permitAll()
-                            .requestMatchers("/candidate/auth").permitAll();
+                            .requestMatchers("/candidate/auth").permitAll()
+                            .requestMatchers(SWAGGER_LIST).permitAll();
                     auth.anyRequest().authenticated();
 
                 })
