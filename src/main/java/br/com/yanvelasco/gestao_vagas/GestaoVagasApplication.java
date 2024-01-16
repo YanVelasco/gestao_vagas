88a58e1a0d4a01bf5407c100880df7eb57f22862
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-16 17:13:38 -0300

    Criando SwaggerConfig

--- src/main/java/br/com/yanvelasco/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/GestaoVagasApplication.java
@@ -9,14 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Gestão de Vagas",
-				description = "API responsável pela gestão de vagas",
-				version = "1"
-		)
-)
-@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
