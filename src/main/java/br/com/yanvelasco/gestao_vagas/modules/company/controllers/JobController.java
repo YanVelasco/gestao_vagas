843deb3b3a5e8ed9e801a5a2aa31cf1793c85c27
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-16 11:16:44 -0300

    Inserindo exemplos nas requisições

--- src/main/java/br/com/yanvelasco/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,13 @@ package br.com.yanvelasco.gestao_vagas.modules.company.controllers;
 import br.com.yanvelasco.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.yanvelasco.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.yanvelasco.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +31,12 @@ public class JobController {
 
     @PostMapping
     @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Informações das Vagas")
+    @Operation(summary = "Cadastro de Vagas", description = "Essa função é resposável por cadastrar vagas")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = JobEntity.class))
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> create(@RequestBody @Valid CreateJobDTO createJobDTO, HttpServletRequest httpServletRequest){
         System.out.println(createJobDTO);
         try {
--- src/main/java/br/com/yanvelasco/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,7 +1,15 @@
 package br.com.yanvelasco.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CreateJobDTO(
+
+        @Schema(example = "Vaga pra pessoa desenvolvedora Júnior", requiredMode = Schema.RequiredMode.REQUIRED)
         String description,
+
+        @Schema(example = "GYMpass, VA, VR", requiredMode = Schema.RequiredMode.REQUIRED)
         String benefits,
+
+        @Schema(example = "Júnior, Pleno, Sênior", requiredMode = Schema.RequiredMode.REQUIRED)
         String level) {
 }
