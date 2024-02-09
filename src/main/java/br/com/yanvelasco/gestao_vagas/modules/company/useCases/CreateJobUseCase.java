a5bdf3df68fb2183c8096d4401eb403774de91b0
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-02-09 15:40:53 -0300

    JobController: Finalizando caso de teste com exceção

--- src/main/java/br/com/yanvelasco/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/br/com/yanvelasco/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,6 +1,8 @@
 package br.com.yanvelasco.gestao_vagas.modules.company.usecases;
 
+import br.com.yanvelasco.gestao_vagas.exceptions.NotFound;
 import br.com.yanvelasco.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.yanvelasco.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.yanvelasco.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,13 @@ public class CreateJobUseCase {
     @Autowired
     private JobRepository jobRepository;
 
-    @SuppressWarnings("null")
+    @Autowired
+    private CompanyRepository companyRepository;
+
     public JobEntity execute(JobEntity jobEntity){
+        companyRepository.findById(jobEntity.getCompanyId()).orElseThrow(
+            () -> new NotFound("Empresa não encontrada")
+        );
         return jobRepository.save(jobEntity);
     }
 
--- src/test/java/br/com/yanvelasco/gestao_vagas/modules/compny/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/yanvelasco/gestao_vagas/modules/compny/controllers/CreateJobControllerTest.java
@@ -1,5 +1,7 @@
 package br.com.yanvelasco.gestao_vagas.modules.compny.controllers;
 
+import java.util.UUID;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +18,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import br.com.yanvelasco.gestao_vagas.exceptions.NotFound;
+import static org.assertj.core.api.Assertions.assertThat;
 import br.com.yanvelasco.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.yanvelasco.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.yanvelasco.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -26,48 +30,68 @@ import br.com.yanvelasco.gestao_vagas.modules.utils.TestUtils;
 @ActiveProfiles("test")
 public class CreateJobControllerTest {
 
-    private MockMvc mockMvc;
+        private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+        @Autowired
+        private WebApplicationContext webApplicationContext;
 
-    @Autowired
-    private CompanyRepository companyRepository;
+        @Autowired
+        private CompanyRepository companyRepository;
 
-    @Before
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
-    }
+        @Before
+        public void setup() {
+                mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                                .apply(SecurityMockMvcConfigurers.springSecurity())
+                                .build();
+        }
 
-    @Test
-    public void should_be_able_to_create_a_new_job() throws Exception {
+        @Test
+        public void should_be_able_to_create_a_new_job() throws Exception {
 
-        var company = CompanyEntity.builder()
-                .description("COMPANY_DESCRIPTION")
-                .email("email@company.com")
-                .password("12345678")
-                .username("company_username")
-                .name("COMPANY_NAME")
-                .website("http://example.com")
-                .build();
+                var company = CompanyEntity.builder()
+                                .description("COMPANY_DESCRIPTION")
+                                .email("email@company.com")
+                                .password("12345678")
+                                .username("company_username")
+                                .name("COMPANY_NAME")
+                                .website("http://example.com")
+                                .build();
 
-        company = companyRepository.saveAndFlush(company);
+                company = companyRepository.saveAndFlush(company);
 
-        var createJobDTO = CreateJobDTO.builder()
-                .benefits("BENEFITS_TEST")
-                .description("DESCRIPTION_TEST")
-                .level("LEVEL_TEST")
-                .build();
+                var createJobDTO = CreateJobDTO.builder()
+                                .benefits("BENEFITS_TEST")
+                                .description("DESCRIPTION_TEST")
+                                .level("LEVEL_TEST")
+                                .build();
 
-        var result = mockMvc.perform(
-                MockMvcRequestBuilders.post("/company/job")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(TestUtils.objectToJson(createJobDTO))
-                        .header("Authorization", TestUtils.generateToken(company.getId(), "JAVAGAS_@123#")))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                var result = mockMvc.perform(
+                                MockMvcRequestBuilders.post("/company/job")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(TestUtils.objectToJson(createJobDTO))
+                                                .header("Authorization",
+                                                                TestUtils.generateToken(company.getId(),
+                                                                                "JAVAGAS_@123#")))
+                                .andExpect(MockMvcResultMatchers.status().isOk());
 
-        System.out.println(result);
-    }
+                System.out.println(result);
+        }
+
+        @Test
+        public void should_not_be_able_to_create_a_new_job_if_company_not_found() throws Exception {
+                var createJobDTO = CreateJobDTO.builder()
+                                .benefits("BENEFITS_TEST")
+                                .description("DESCRIPTION_TEST")
+                                .level("LEVEL_TEST")
+                                .build();
+
+                mockMvc.perform(
+                                MockMvcRequestBuilders.post("/company/job")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(TestUtils.objectToJson(createJobDTO))
+                                                .header("Authorization",
+                                                                TestUtils.generateToken(UUID.randomUUID(),
+                                                                                "JAVAGAS_@123#")))
+                                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        }
 }
